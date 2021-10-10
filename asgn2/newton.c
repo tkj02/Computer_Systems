6af025b1e4673bde44f689b2c8434290c114cef9
Teresa Joseph <tkjoseph@ucsc.edu>
2021-10-10 04:31:15 +0000

    Made neater

--- asgn2/newton.c
+++ asgn2/newton.c
@@ -1,19 +1,20 @@
 #include "mathlib.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 static int counter = 0;
 
 double sqrt_newton(double x) {
-    double y = 1.0;
-    double z = 0.0;
+    double first_term = 1.0;
+    double second_term = 0.0;
     counter = 0;
-    while (absolute(y - z) > EPSILON) {
-        z = y;
-        y = (y + x / y) / 2;
+    while (absolute(first_term - second_term) > EPSILON) {
+        z = first_term;
+        first_term = (second_term + x / second_term) / 2;
         counter++;
     }
-    return y;
+    return first_term;
 }
 
 int sqrt_newton_iters() {
