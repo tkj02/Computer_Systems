e3580991df80d83f92954af7714d167ef2877461
Teresa Joseph <tkjoseph@ucsc.edu>
2021-10-10 21:14:15 +0000

    e complete

--- asgn2/e.c
+++ asgn2/e.c
@@ -2,21 +2,18 @@
 
 #include <stdio.h>
 
-static int counter = 0;
+static int counter;
 
 double e(void) {
-    int first_term = 1;
-    int second_term = 0;
-    int factorial = 0;
-    int counter = 0;
+    double first_term = 1.0;
+    double second_term = 0.0;
+    double factorial = 1.0;
+    counter = 1;
     while (absolute(first_term - second_term) > EPSILON) {
-        if (factorial == 0) {
-            factorial = 1;
-        } else {
-            second_term = first_term;
-            first_term += (1 / factorial);
-            counter++;
-        }
+        second_term = first_term;
+        factorial *= counter;
+        first_term += 1 / factorial;
+        counter++;
     }
     return first_term;
 }
