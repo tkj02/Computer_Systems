1e9ab4bebb5401a1f55a575941f5919c91d99131
Teresa Joseph <tkjoseph@ucsc.edu>
2021-10-16 01:05:36 +0000

    insert format

---
+++ asgn3/insert.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stats.h"
+
+void insertion_sort(Stats *stats, uint32_t *A, uint32_t n){
+	for (int i = 0; i <= n; i++){
+		int j = i;
+		int temp = A[i];
+		while (j > 0 && temp < A[j-1]){
+			A[j] = A[j-1];
+			j -= 1;
+		}
+		A[j] = temp;
+	}
+}
