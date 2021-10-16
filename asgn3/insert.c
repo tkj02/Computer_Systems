2e90e48106bcad273698d869239ba884bf0bc092
Teresa Joseph <tkjoseph@ucsc.edu>
2021-10-16 18:47:38 +0000

    fixed loop

--- asgn3/insert.c
+++ asgn3/insert.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
 #include "stats.h"
 
 void insertion_sort(Stats *stats, uint32_t *A, uint32_t n){
-	for (int i = 0; i <= n; i++){
+	for (int i = 0; i < n; i++){
 		int j = i;
 		int temp = A[i];
 		while (j > 0 && temp < A[j-1]){
