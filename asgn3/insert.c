fa716d21fdabcb7a54465ec43696b51f72619aea
Teresa Joseph <tkjoseph@ucsc.edu>
2021-10-17 05:45:10 +0000

    moves

--- asgn3/insert.c
+++ asgn3/insert.c
@@ -7,9 +7,9 @@ void insertion_sort(Stats *stats, uint32_t *A, uint32_t n) {
         int j = i;
         int temp = move(stats, A[i]);
         while (j > 0 && temp < A[j - 1]) {
-            A[j] = A[j - 1];
+            A[j] = move(stats, A[j - 1]);
             j -= 1;
         }
-        A[j] = temp;
+        A[j] = move(stats, temp);
     }
 }
