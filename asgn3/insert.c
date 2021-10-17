80546a5c51844c203d05e7aa420fc4ccce624229
Teresa Joseph <tkjoseph@ucsc.edu>
2021-10-17 22:46:48 +0000

    finalized

--- asgn3/insert.c
+++ asgn3/insert.c
@@ -6,7 +6,7 @@ void insertion_sort(Stats *stats, uint32_t *A, uint32_t n) {
     for (int i = 1; i < n; i++) {
         int j = i;
         int temp = move(stats, A[i]);
-        while (j>0 && cmp(stats, temp, A[j - 1])<0) {
+        while (j > 0 && cmp(stats, temp, A[j - 1]) < 0) {
             A[j] = move(stats, A[j - 1]);
             j -= 1;
         }
