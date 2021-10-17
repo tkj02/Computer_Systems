9505d9568ca111a9df07293a279898c9acbe6a98
Teresa Joseph <tkjoseph@ucsc.edu>
2021-10-17 23:57:57 +0000

    finalized

--- asgn3/shell.c
+++ asgn3/shell.c
@@ -5,27 +5,31 @@
 
 static uint32_t i = 0;
 
-uint32_t gaps(uint32_t n) {
-    if (i <= 0) {
-        i = (uint32_t)(log(3 + 2 * n) / log(3));
-    } else {
-        --i;
-    }
-    return (uint32_t)((pow(3, i) - 1) / 2);
+static uint32_t gap_array[32];
+
+uint32_t array_size;
+
+void new_gap(uint32_t n){
+	uint32_t range = (uint32_t)(log(3 + 2 * n) / log(3));
+	uint32_t index = 0; 
+	for (uint32_t i = range; i > 0; --i){
+		gap_array[index++] = (uint32_t)((pow(3, i) - 1) / 2);
+	}
+	array_size = range;
 }
 
 void shell_sort(Stats *stats, uint32_t *A, uint32_t n) {
-    uint32_t iters = (log(3 + 2 * n) / log(3));
-    for (uint32_t g = 0; g < iters; g++) {
-        uint32_t gap = gaps(n);
-        for (uint32_t i = gap; i < n; i++) {
-            uint32_t j = i;
-            uint32_t temp = move(stats, A[i]);
-            while (j >= g && temp < A[j - g]) {
-                A[j] = move(stats, A[j - g]);
-                j -= g;
-            }
-            A[j] = move(stats, temp);
-        }
-    }
+	new_gap(n);
+	for(uint32_t ii = 0; ii < array_size; ii++){
+		uint32_t gap = gap_array[ii];
+		for(uint32_t i = gap; i < n; i++){
+			uint32_t j = i;
+			uint32_t temp = move(stats, A[i]);
+			while (j >= gap && cmp(stats, temp, A[j-gap])<0){
+				A[j] = move(stats, A[j-gap]);
+				j -= gap;
+			}
+			A[j] = move(stats, temp);
+		} 
+	}
 }
