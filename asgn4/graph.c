45f84aa67c2306549baaf346a027666bbd78f582
Teresa Joseph <tkjoseph@ucsc.edu>
2021-10-23 22:32:49 +0000

    graph (mostly) complete

--- asgn4/graph.c
+++ asgn4/graph.c
@@ -1,59 +1,81 @@
 #include "vertices.h"
 
-#include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct Graph {
-	uint32_t vertices; // Number of vertices.
-	bool undirected; // Undirected graph?
-	bool visited[VERTICES]; // Where have we gone?
-	uint32_t matrix[VERTICES][VERTICES]; // Adjacency matrix.
-};
+typedef struct Graph {
+    uint32_t vertices; // Number of vertices.
+    bool undirected; // Undirected graph?
+    bool visited[VERTICES]; // Where have we gone?
+    uint32_t matrix[VERTICES][VERTICES]; // Adjacency matrix.
+} Graph;
 
-Graph *graph_create(uint32_t vertices, bool undirected){
-	Graph *G = (Graph *)calloc(1, sizeof(Graph));
-	G -> vertices = vertices;
-	G -> undirected = undirected;
-	return G;
+Graph *graph_create(uint32_t vertices, bool undirected) {
+    Graph *G = (Graph *)calloc(1, sizeof(Graph));
+    if(G){
+	    G->vertices = vertices;
+	    G->undirected = undirected;
+    }
+    return G;
 }
 
-void graph_delete(Graph **G){
-	free(*G);
-	*G = NULL;
-	return;
+void graph_delete(Graph **G) {
+    void free(void *G);
+    *G = NULL;
+    return;
+}
+uint32_t graph_vertices(Graph *G) {
+    return G->vertices;
 }
 
-uint32_t graph_vertices(Graph *G){
-	G -> vertices = vertices;
-	return G;
+bool graph_add_edge(Graph *G, uint32_t i, uint32_t j, uint32_t k) {
+    if (i > G->vertices || j > G->vertices) {
+        return false;
+    }
+    G->matrix[i][j] = k;
+    return true;
 }
 
-bool graph_add_edge(Graph *G, uint32_t i, uint32_t j, uint32_t k){
-	return;
+bool graph_has_edge(Graph *G, uint32_t i, uint32_t j) {
+    if (i < G->vertices && j < G->vertices && G->matrix[i][j] > 0){
+        return true;
+    }
+    return false;
 }
 
-bool graph_has_edge(Graph *G, uint32_t i, uint32_t j){
-	return;
+uint32_t graph_edge_weight(Graph *G, uint32_t i, uint32_t j) {
+    if (i < G->vertices && j < G->vertices && G->matrix[i][j] > 0){
+	return G->matrix[i][j];
+    }
+        return 0;
 }
 
-uint32_t graph_edge_weight(Graph *G, uint32_t i, uint32_t j){
-        return;
+bool graph_visited(Graph *G, uint32_t v) {
+    if (G->visited[v]){
+    	return true;
+    }
+    return false;
 }
 
-bool graph_visited(Graph *G, uint32_t v){
-        return;
+void graph_mark_visited(Graph *G, uint32_t v) {
+    if (v > G->vertices){
+        G->visited[v] = true;
+    }
 }
 
-void graph_mark_visited(Graph *G, uint32_t v){
-        return;
+void graph_mark_unvisited(Graph *G, uint32_t v) {
+    if (v > G->vertices){
+        G->visited[v] = false;
+    }
 }
 
-void graph_mark_unvisited(Graph *G, uint32_t v){
-        return;
+void graph_print(Graph *G) {
+	for (uint32_t i = 0; i < G->vertices; i++){
+		for (uint32_t j = 0; j < G->vertices; j++){
+			printf("%u ", G->matrix[i][j]);
+		}
+		printf("\n");
+	}
 }
-
-void graph_print(Graph *G){
-        return;
-}
-
