b0f6482f3ab91b8b1b4673f02ed2fb2ac3b5cda5
Teresa Joseph <tkjoseph@ucsc.edu>
2021-10-24 03:59:47 +0000

    finalized

--- asgn4/stack.c
+++ asgn4/stack.c
@@ -33,17 +33,17 @@ void stack_delete(Stack **s) {
 }
 
 bool stack_empty(Stack *s) {
-    if (s->top == 0){
-    	return true;
+    if (s->top == 0) {
+        return true;
     }
     return false;
 }
 
 bool stack_full(Stack *s) {
-	if (s->top >= s->capacity){
-		return true;
-	}
-	return false;
+    if (s->top >= s->capacity) {
+        return true;
+    }
+    return false;
 }
 
 uint32_t stack_size(Stack *s) {
@@ -51,46 +51,45 @@ uint32_t stack_size(Stack *s) {
 }
 
 bool stack_push(Stack *s, uint32_t x) {
-	if(stack_full(s)){
-		return false;
-	} 
-	s->items[s->top] = x;
-	s->top++;
-	return true;
+    if (stack_full(s)) {
+        return false;
+    }
+    s->items[s->top] = x;
+    s->top++;
+    return true;
 }
 
 bool stack_pop(Stack *s, uint32_t *x) {
-	if(stack_empty(s)){
-		return false;
-	}
-	s->top--;
-	*x = s->items[s->top];
-	return true;
+    if (stack_empty(s)) {
+        return false;
+    }
+    s->top--;
+    *x = s->items[s->top];
+    return true;
 }
 
 bool stack_peek(Stack *s, uint32_t *x) {
-    if (stack_empty(s)){
-    	return false;
+    if (stack_empty(s)) {
+        return false;
     }
-    *x = s->items[s->top-1];
+    *x = s->items[s->top - 1];
     return true;
 }
 
 void stack_copy(Stack *dst, Stack *src) {
-   dst->capacity = src->capacity;
-   dst->top = src->top;
-   for(uint32_t i = 0; i < src->top; i++){
-   	dst->items[i] = src->items[i];
-   }
+    dst->capacity = src->capacity;
+    dst->top = src->top;
+    for (uint32_t i = 0; i < src->top; i++) {
+        dst->items[i] = src->items[i];
+    }
 }
 
 void stack_print(Stack *s, FILE *outfile, char *cities[]) {
     for (uint32_t i = 0; i < s->top; i += 1) {
         fprintf(outfile, "%s", cities[s->items[i]]);
-	if (i + 1 != s->top) {
+        if (i + 1 != s->top) {
             fprintf(outfile, " -> ");
         }
     }
     fprintf(outfile, "\n");
-
 }
