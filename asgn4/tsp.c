6c8d0a117017ef49ca4d7b099e2abe9987f85f2e
Teresa Joseph <tkjoseph@ucsc.edu>
2021-10-24 04:45:42 +0000

    help

--- asgn4/tsp.c
+++ asgn4/tsp.c
@@ -1,68 +1,67 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <inttypes.h>
-#include <unistd.h>
-
 #include "graph.h"
 #include "path.h"
 #include "stack.h"
 #include "vertices.h"
+
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define OPTIONS "hvui:o:"
 
 static FILE *input = NULL;
 static FILE *output = NULL;
 
-int main(int argc, char** argv){
-	int opt = 0;
-	bool no_input = true;
-	bool undirected = false;
-	uint32_t vertices = 0;
-	char *input_file = NULL;
-	char *output_file = NULL;
+int main(int argc, char **argv) {
 
-	while ((opt = getopt(argc, argv, OPTIONS)) != -1){
-		no_input = false;
-		switch(opt){
-			case 'h':
-				//print message
-			case 'v':
-				//print all paths
-			case 'u':
-				//specify graph to be undirected
-			case 'i':
-				//specify input file
-				input_file = optarg;
-				break;
-			case 'o':
-				//specify output file
-				input_file = optarg;
-				break;
-		}
-	}
+    int opt = 0;
+    bool no_input = true;
+    bool undirected = false;
+    uint32_t vertices = 0;
+    char *input_file = NULL;
+    //    char *output_file = NULL;
 
-	input = stdin;
-	output = stdout;
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+        no_input = false;
+        switch (opt) {
+        case 'h':
+            printf("SYNOPSIS\n  Traveling Salesman Problem using DFS.\n\n");
+            printf("USAGE\n  ./tsp [-u] [-v] [-h] [-i infile] [-o outfile]\n\n");
+            printf("OPTIONS\n");
+            printf("  -u             Use undirected graph.\n");
+            printf("  -v             Enable verbose printing.\n");
+            printf("  -h             Program usage and help.\n");
+            printf("  -i infile      Input containing graph (default: stdin)\n");
+            printf("  -o outfile     Output of computed path (default: stdout)\n");
+        case 'v':
+            //print all paths
+        case 'u':
+            //specify graph to be undirected
+        case 'i':
+            //specify input file
+            input_file = optarg;
+            break;
+        case 'o':
+            //specify output file
+            input_file = optarg;
+            break;
+        }
+    }
 
-	Graph *locations = graph_create(vertices, undirected);
-	
-	if (locations == NULL){
-		//print error to stderr, clean memory, delete functions
-		//return 1 
-	}
-	//call path_create, call dfs
-	//print shortest path
-	//clean memory
-	
-	return 0;
+    input = stdin;
+    output = stdout;
+
+    Graph *locations = graph_create(vertices, undirected);
+
+    if (locations == NULL) {
+        //print error to stderr, clean memory, delete functions
+        //return 1
+    }
+    //call path_create, call dfs
+    //print shortest path
+    //clean memory
+
+    return 0;
 }
-
-
-
-
-
-
-
-
-
