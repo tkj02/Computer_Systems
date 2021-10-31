4c282ba550a57ad580d98e2b2a0b14e06630100a
Teresa Joseph <tkjoseph@ucsc.edu>
2021-10-31 05:20:59 +0000

    fixes

--- asgn5/node.c
+++ asgn5/node.c
@@ -11,39 +11,38 @@ struct Node {
     uint64_t frequency;
 };
 
-Node *node_create(uint8_t symbol, uint64_t frequency){
-	Node *n = (Node *) malloc(sizeof(Node));
-	if (n){
-		n->left = NULL;
-		n->right = NULL;
-		n->symbol = symbol;
-		n->frequency = frequency;
-	}
-	return n;
+Node *node_create(uint8_t symbol, uint64_t frequency) {
+    Node *n = (Node *) malloc(sizeof(Node));
+    if (n) {
+        n->left = NULL;
+        n->right = NULL;
+        n->symbol = symbol;
+        n->frequency = frequency;
+    }
+    return n;
 }
 
-void node_delete(Node **n){
+void node_delete(Node **n) {
     void free(void *n);
     *n = NULL;
 }
 
-Node *node_join(Node *left, Node *right){
-	left->frequency = left_freq;
-        right->frequency = right_freq;
-	Node *new_node = node_create('$', left_freq+right_freq);
-	return new_node;
+Node *node_join(Node *left, Node *right) {
+    left->frequency = left_freq;
+    right->frequency = right_freq;
+    Node *new_node = node_create('$', left_freq + right_freq);
+    return new_node;
 }
 
-void node_print(Node *n){
-	printf("node symbol: %d", n->symbol);
-	printf("node freq: %d", n->frequency);
-	if (n->left != NULL && n->right != NULL){
-		n->left = left;
-		n->right = right;
-		printf("node's left symbol: %d", left->symbol);
-		printf("node's right symbol: %d", right->symbol);
-		printf("node's left freq: %lu", left->frequency);
-		printf("node's right freq: %lu", right->frequency);
-	}
+void node_print(Node *n) {
+    printf("node symbol: %d", n->symbol);
+    printf("node freq: %lu", n->frequency);
+    if (n->left != NULL && n->right != NULL) {
+        n->left = left;
+        n->right = right;
+        printf("node's left symbol: %d", left->symbol);
+        printf("node's right symbol: %d", right->symbol);
+        printf("node's left freq: %lu", left->frequency);
+        printf("node's right freq: %lu", right->frequency);
+    }
 }
-
