544fb2f0264b4b89ed2039f1fb6b4d3217a098c2
Teresa Joseph <tkjoseph@ucsc.edu>
2021-11-02 07:21:10 +0000

    starting decoder

---
+++ asgn5/decode.c
@@ -0,0 +1,14 @@
+#include "pq.h"
+#include "code.h"
+#include "node.h"
+#include "stack.h"
+#include "io.h"
+#include "huffman.h"
+
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
