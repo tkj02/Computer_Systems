23224c0fff023cedbb14e7965ffe0410dd16dfab
Teresa Joseph <tkjoseph@ucsc.edu>
2021-11-04 21:05:50 +0000

    write and flush code

--- asgn5/io.c
+++ asgn5/io.c
@@ -30,25 +30,50 @@ int write_bytes(int outfile, uint8_t *buf, int nbytes) {
 }
 
 bool read_bit(int infile, uint8_t *bit) {
-    static uint8_t buffer_array[BLOCK] = {0};
-    static uint32_t bit_top = 0;
-    static uint32_t bit_buffer_size = 0;
-    if (bit_top == 0) {
-        bit_buffer_size = read_bytes(infile, buffer_array, BLOCK);
-        if (bit_buffer_size == 0) {
+    static uint8_t buffer_array[BLOCK] = { 0 };
+    static uint32_t index = 0;
+    static uint32_t end = -1;
+    if (index == 0) {
+        uint32_t bits_read = read_bytes(infile, buffer_array, BLOCK);
+        if (bits_read < BLOCK) {
+            end = bits_read * 8 + 1;
+        }
+        if (bits_read == 0) {
             return false;
         }
     }
-    *bit = buffer_array[bit_buffer_size];
-    bit_top++;
-    if (bit_top == bit_buffer_size * 8) {
-        bit_top = 0;
+    *bit = buffer_array[index];
+    index++;
+    if (index == BLOCK * 8) {
+        index = 0;
     }
-    return true;
+    if (index == end) {
+        return true;
+    }
+    return false;
 }
 
+static uint8_t buffer[BLOCK] = { 0 };
+static uint32_t index = 0;
+
 void write_code(int outfile, Code *c) {
+    for (uint32_t i = 0; i < code_size(c); i++) {
+        uint32_t bit = code_get_bit(c, i);
+        if (bit == 1) {
+            bit = index;
+        } else {
+            bit = 0;
+        }
+        index++;
+        if (index == BLOCK) {
+            flush_codes(outfile);
+        }
+    }
 }
 
 void flush_codes(int outfile) {
+    if (index > 0) {
+        //convert index to bytes
+        write_bytes(outfile, buffer, BLOCK);
+    }
 }
