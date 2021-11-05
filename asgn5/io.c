dbbdc3857addc9fd67d08fe2b144f102b7b9006c
Teresa Joseph <tkjoseph@ucsc.edu>
2021-11-05 08:46:57 +0000

    finalizing io functions

--- asgn5/io.c
+++ asgn5/io.c
@@ -8,23 +8,27 @@
 #include <unistd.h>
 
 int read_bytes(int infile, uint8_t *buf, int nbytes) {
-    uint64_t current_bytes_read = 0;
-    while (read(infile, buf + bytes_read, nbytes - bytes_read > 0)) {
-        bytes_read += current_bytes_read;
-        if (bytes_read == (uint64_t) nbytes) {
+    int current_bytes_read = 0;
+    int bytes_read = 0;
+    while (bytes_read < nbytes) {
+        current_bytes_read = read(infile, buf + bytes_read, nbytes - bytes_read);
+        if (current_bytes_read < 1) {
             break;
         }
+        bytes_read += current_bytes_read;
     }
     return bytes_read;
 }
 
 int write_bytes(int outfile, uint8_t *buf, int nbytes) {
-    uint64_t current_bytes_written = 0;
-    while (write(outfile, buf + bytes_written, nbytes - bytes_written > 0)) {
-        bytes_written += current_bytes_written;
-        if (bytes_written == (uint64_t) nbytes) {
+    int current_bytes_written = 0;
+    int bytes_written = 0;
+    while (bytes_written < nbytes) {
+        current_bytes_written = write(outfile, buf + bytes_written, nbytes - bytes_written);
+        if (current_bytes_written < 1) {
             break;
         }
+        bytes_written += current_bytes_written;
     }
     return bytes_written;
 }
@@ -35,22 +39,32 @@ bool read_bit(int infile, uint8_t *bit) {
     static uint32_t end = -1;
     if (index == 0) {
         uint32_t bits_read = read_bytes(infile, buffer_array, BLOCK);
+        //printf("bits read %u\n", bits_read);
         if (bits_read < BLOCK) {
             end = bits_read * 8 + 1;
         }
-        if (bits_read == 0) {
+        if (bits_read <= 0) {
             return false;
         }
     }
-    *bit = buffer_array[index];
+    int byte_index = index / 8;
+    int bit_index = index % 8;
+    uint8_t temp = buffer_array[byte_index];
+    uint8_t mask = 1 << bit_index;
+    //printf("bi %d byi %d temp %d \n", bit_index, byte_index, temp);
+    if ((temp & mask) != 0) {
+        *bit = 1;
+    } else {
+        *bit = 0;
+    }
     index++;
     if (index == BLOCK * 8) {
         index = 0;
     }
     if (index == end) {
-        return true;
+        return false;
     }
-    return false;
+    return true;
 }
 
 static uint8_t buffer[BLOCK] = { 0 };
@@ -59,21 +73,25 @@ static uint32_t index = 0;
 void write_code(int outfile, Code *c) {
     for (uint32_t i = 0; i < code_size(c); i++) {
         uint32_t bit = code_get_bit(c, i);
+        int byte_index = index / 8;
+        int bit_index = index % 8;
+        uint8_t mask = 1 << bit_index;
+        buffer[byte_index] = buffer[byte_index] & (~mask);
+
         if (bit == 1) {
-            bit = index;
-        } else {
-            bit = 0;
+            buffer[byte_index] = buffer[byte_index] | mask;
         }
         index++;
-        if (index == BLOCK) {
-            flush_codes(outfile);
+        if (index == BLOCK * 8) {
+            write_bytes(outfile, buffer, BLOCK);
+            index = 0;
         }
     }
 }
 
 void flush_codes(int outfile) {
     if (index > 0) {
-        //convert index to bytes
-        write_bytes(outfile, buffer, BLOCK);
+        write_bytes(outfile, buffer, index / 8);
     }
+    index = 0;
 }
