2a7bc9e9d80d718301fd75b28d531ffdea9a4f18
Teresa Joseph <tkjoseph@ucsc.edu>
2021-11-07 07:05:09 +0000

    testing decoding process here temporarily

--- asgn5/huffman.c
+++ asgn5/huffman.c
@@ -59,24 +59,43 @@ Node *build_tree(uint64_t hist[static ALPHABET]) {
     }
     return NULL;
 }
+int recur_count1 = 0;
+int tree_depth = 0;
 
 void building_codes(Node *root, Code table[static ALPHABET], Code *c) {
     uint8_t popped_bit;
-    if (root->symbol != '$') {
+    tree_depth++;
+    if (root == NULL)
+        return;
+    //if (root->symbol != '$') {
+    if (root->left == NULL && root->right == NULL) {
         table[root->symbol] = *c;
+        recur_count1++;
+        //yy	printf("Adding code for 0x%02x Count: %d Depth:%d \n", root->symbol, recur_count1, tree_depth);
+        tree_depth--;
         return;
     }
     code_push_bit(c, 0);
+    if (root->left == NULL) {
+        printf("Left = NULL  0x%02x Count: %d Depth:%d \n", root->symbol, recur_count1, tree_depth);
+    }
+    if (root->right == NULL) {
+        printf(
+            "Rightt = NULL  0x%02x Count: %d Depth:%d \n", root->symbol, recur_count1, tree_depth);
+    }
     building_codes(root->left, table, c);
     code_pop_bit(c, &popped_bit);
     code_push_bit(c, 1);
     building_codes(root->right, table, c);
     code_pop_bit(c, &popped_bit);
+    tree_depth--;
 }
+Code g_c;
 
 void build_codes(Node *root, Code table[static ALPHABET]) {
-    Code c = code_init();
-    building_codes(root, table, &c);
+    //Code c = code_init();
+    g_c = code_init();
+    building_codes(root, table, &g_c);
 }
 
 #if 0
@@ -90,26 +109,29 @@ void delete_tree(Node **root) {
 }
 #endif
 
-bool decode(Node *root, int infile, int outfile){
-	if (root == NULL){
-		return false;
-	}
-	if (root->symbol != '$'){
-		write(outfile, &root->symbol, 1);
-		return true;
-	}
-	uint8_t bit;
-	bool flag;
-	flag = read_bit(infile, &bit);
-	if (flag == false){
-		return false;
-	}
-	if (bit == 0){
-		decode(root->left, infile, outfile);
-	}
-	else{
-		decode(root->right, infile, outfile);
-	}
-	return true;
-}
+int recur_count = 0;
 
+bool decode(Node *root, int infile, int outfile) {
+
+    if (root == NULL) {
+        return false;
+    }
+    if (root->symbol != '$') {
+        write(outfile, &root->symbol, 1);
+        recur_count++;
+        //printf("recursive count %d\n",recur_count);
+        return true;
+    }
+    uint8_t bit;
+    bool flag;
+    flag = read_bit(infile, &bit);
+    if (flag == false) {
+        return false;
+    }
+    if (bit == 0) {
+        return decode(root->left, infile, outfile);
+    } else {
+        return decode(root->right, infile, outfile);
+    }
+    return false;
+}
