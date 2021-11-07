232cb43f1f77dc962753bc51d1264b5abe4f7cd4
Teresa Joseph <tkjoseph@ucsc.edu>
2021-11-07 23:33:27 +0000

    updating command line

--- asgn5/encode.c
+++ asgn5/encode.c
@@ -18,11 +18,151 @@
 
 #define OPTIONS "hi:o:v"
 
+// Protoypes of functions that will be used
+// create_hist and get_unique_count are under main()
+// decode is under huffman.c
+int create_hist(int fd);
+int get_unique_count();
 bool decode(Node *root, int infile, int outfile);
 
+// Global variables
+// Initializes histogram and code table
 uint64_t g_hist[ALPHABET];
 Code g_table[ALPHABET];
 
+// Main function
+int main(int argc, char **argv) {
+
+    // Creates necessary structs/variables
+    // fileStat is for reading/writing
+    // pq is for enqueuing and dequeuing
+    // hdr is for header data
+    struct stat fileStat;
+    PriorityQueue *pq = pq_create(ALPHABET);
+    Header hdr;
+
+    // Parses through command line options
+    int opt = 0;
+    int fi = 0;
+    int fout = 1;
+    uint64_t in_size;
+    uint64_t out_size = 0;
+    float temp;
+    bool v_flag = false;
+
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+        switch (opt) {
+	
+	// Prints help message
+        case 'h':
+            printf("SYNOPSIS\n  A Huffman encoder.\n  Compresses a file using the Huffman coding "
+                   "algorithm.\n\n");
+            printf("USAGE\n  ./encode [-h] [-i infile] [-o outfile]\n\n");
+            printf("OPTIONS\n");
+            printf("  -h             Program usage and help.\n");
+            printf("  -v             Print compression statistics.\n");
+            printf("  -i infile      Input file to compress.\n");
+            printf("  -o outfile     Output of compressed data.\n");
+            break;
+	
+	    // Prints compression statistics
+	case 'v':
+	    v_flag = true;
+	    break;
+
+	// Opens infile
+        case 'i':
+	    fi = open(optarg, O_RDONLY);
+	    if (fi == -1){
+	    	printf("file open for reading failed\n");
+		return -1;
+	    }
+	    break;
+	
+	// Opens outfile
+        case 'o':
+	    fout = open(optarg, O_RDWR | O_CREAT);
+            if (fout == -1) {
+                printf("file open for write failed for\n");
+                return -1;
+            }
+            fstat(fi, &fileStat);
+	    in_size = fileStat.st_size;
+            fchmod(fout, fileStat.st_mode);
+	    break;
+        }
+    }
+
+    // Creates histogram and sets all values to zero
+    create_hist(fi);
+
+    // Creates nodes based on histogram
+    // Enqueues each node to priority queue
+    for (int i = 0; i < 256; i++) {
+        if (g_hist[i] == 0) {
+            continue;
+        }
+        Node *node = node_create(i, g_hist[i]);
+        enqueue(pq, node);
+    }
+
+    // Creates root node of Huffman tree based on histogram
+    Node *hroot = build_tree(g_hist);
+    if (hroot == NULL) {
+        printf("error failed huff tree\n");
+        return -1;
+    }
+
+    // Creates codes for each node
+    build_codes(hroot, g_table);
+
+    lseek(fi, 0, SEEK_SET);
+
+    uint8_t *buffer = malloc(BLOCK);
+    if (buffer == NULL) {
+        printf("error allocating\n");
+        return -1;
+    }
+
+    // Assignment for header members
+    hdr.magic = MAGIC;
+    hdr.permissions = fileStat.st_mode;
+    hdr.tree_size = (3 * get_unique_count()) - 1;
+    hdr.file_size = fileStat.st_size;
+
+    // Write bytes of header to outfile
+    write_bytes(fout, (uint8_t *) &hdr, sizeof(Header));
+
+    // 
+    dump_tree(fout, hroot);
+
+    while (1) {
+        uint32_t return_len = read(fi, buffer, BLOCK);
+        if (return_len < 1) {
+            break;
+        }
+        for (uint32_t i = 0; i < return_len; i++) {
+            write_code(fout, &g_table[buffer[i]]);
+        }
+    }
+    fstat(fout, &fileStat);
+    out_size = fileStat.st_size;
+
+    flush_codes(fout);
+    free(buffer);
+    close(fout);
+    close(fi);
+
+    if (v_flag){
+    	temp = 100.0*(1.0-((float)out_size/(float)in_size));
+        printf("input size %lu output size %lu space saving %f\n",
+                            in_size, out_size, temp);
+    }
+    return 0;
+}
+
+// create_hist function
+// Creates the histogram for tracking frequency
 int create_hist(int fd) {
     char *buffer = malloc(BLOCK);
     if (buffer == NULL) {
@@ -48,6 +188,9 @@ int create_hist(int fd) {
     return 0;
 }
 
+// get_unique_count function
+// Gets the number of non-zero symbols in the histogram
+// This is used for the header tree_size
 int get_unique_count() {
     int unique_count = 0;
     for (int i = 0; i < ALPHABET; i++) {
@@ -58,192 +201,3 @@ int get_unique_count() {
     }
     return unique_count;
 }
-
-int main(int argc, char **argv) {
-
-    struct stat fileStat;
-
-    int fd = open("test.txt", O_RDONLY);
-    if (fd == -1) {
-        printf("error not found\n");
-        return -1;
-    }
-    PriorityQueue *pq = pq_create(ALPHABET);
-    create_hist(fd);
-    for (int i = 0; i < 256; i++) {
-        if (g_hist[i] == 0) {
-            continue;
-        }
-        printf("%3d %lu\n", i, g_hist[i]);
-        Node *node = node_create(i, g_hist[i]);
-        enqueue(pq, node);
-    }
-    printf("Creating Tree \n");
-    Node *hroot = build_tree(g_hist);
-    if (hroot == NULL) {
-        printf("error failed huff tree\n");
-        return -1;
-    }
-    printf("Creating Codes 222222 \n");
-    build_codes(hroot, g_table);
-    printf("Created Code 33333\n");
-    for (int i = 0; i < 256; i++) {
-        if (code_size(&g_table[i]) == 0) {
-            continue;
-        }
-        printf("symbol %02x (%c) CodeCount: %d freq: %lu\n", i, (i > ' ' && i < 128) ? i : ' ',
-            code_size(&g_table[i]), g_hist[i]);
-    }
-    close(fd);
-    fd = open("test.txt", O_RDONLY);
-    if (fd == -1) {
-        printf("error not found\n");
-        return -1;
-    }
-
-    fstat(fd, &fileStat);
-
-    int fw = open("test.out", O_RDWR | O_CREAT);
-    if (fw == -1) {
-        printf("error write file open\n");
-        return -1;
-    }
-    fchmod(fw, fileStat.st_mode);
-
-    uint8_t *buffer = malloc(BLOCK);
-    if (buffer == NULL) {
-        printf("error allocating\n");
-        return -1;
-    }
-    Header hdr;
-    hdr.magic = MAGIC;
-    hdr.permissions = fileStat.st_mode;
-    hdr.tree_size = (3 * get_unique_count()) - 1;
-    hdr.file_size = fileStat.st_size;
-
-    write_bytes(fw, (uint8_t *) &hdr, sizeof(Header));
-
-    dump_tree(fw, hroot);
-
-    while (1) {
-        uint32_t return_len = read(fd, buffer, BLOCK);
-        if (return_len < 1) {
-            break;
-        }
-        for (uint32_t i = 0; i < return_len; i++) {
-            write_code(fw, &g_table[buffer[i]]);
-        }
-    }
-    flush_codes(fw);
-    free(buffer);
-    close(fw);
-    close(fd);
-    fd = open("test.out", O_RDONLY);
-    if (fd == -1) {
-        printf("error no test.out for decode\n");
-        return -1;
-    }
-#if 0
-    int bit_count = 0;
-    uint8_t tbit;
-    while(1){
-    	if (read_bit(fd, &tbit) == false){
-		break;
-	}
-	bit_count++;
-    }
-    printf("%d\n", bit_count);
-    return 0;
-#endif
-    fw = open("decode.out", O_RDWR | O_CREAT);
-    if (fw == -1) {
-        printf("error decode file open\n");
-        return -1;
-    }
-    fchmod(fw, fileStat.st_mode);
-    int call_count = 0;
-    while (1) {
-        if (decode(hroot, fd, fw) == false) {
-            break;
-        }
-        call_count++;
-        //printf("call count %d\n", call_count);
-    }
-    close(fd);
-    close(fw);
-
-#if 0
-    int q_count = pq_size(pq);
-
-    for (int i = 0; i < q_count; i++) {
-        Node *node;
-        bool stat = dequeue(pq, &node);
-        if (stat == false) {
-            printf("failed\n");
-        }
-        printf(" sorted %3d %lu\n", node->symbol, node->frequency);
-    }
-#endif
-
-    return 0;
-
-    uint8_t bit;
-    int count = 0;
-    Code c = code_init();
-    while (read_bit(fd, &bit)) {
-        if (count % 8 == 0) {
-            printf("\n");
-        }
-        count++;
-        printf("%d %x\n", count, bit);
-        if (count > 100) {
-            break;
-        }
-        code_push_bit(&c, bit);
-    }
-    uint32_t size = code_size(&c);
-    printf("%u\n", size);
-    for (uint32_t i = 0; i < size + 5; i++) {
-        if (code_pop_bit(&c, &bit) == false) {
-            printf("%d error\n", i);
-        }
-        printf("%d bit: %d\n", i, bit);
-    }
-    return 0;
-
-    int opt = 0;
-    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
-        switch (opt) {
-        case 'h':
-            printf("SYNOPSIS\n  A Huffman encoder.\n  Compresses a file using the Huffman coding "
-                   "algorithm.\n\n");
-            printf("USAGE\n  ./encode [-h] [-i infile] [-o outfile]\n\n");
-            printf("OPTIONS\n");
-            printf("  -h             Program usage and help.\n");
-            printf("  -v             Print compression statistics.\n");
-            printf("  -i infile      Input file to compress.\n");
-            printf("  -o outfile     Output of compressed data.\n");
-            break;
-        case 'v': break;
-        case 'i': break;
-        case 'o': break;
-        }
-    }
-}
-
-// read infile
-//	compute frequency of each symbol
-//	make histogram
-// make tree with histogram
-//	use pq
-// make code table
-// 	index of table represent symbol
-// 	value at index represents symbol's code
-// 	use stack of bits and traverse tree
-// write tree to outfile
-// 	use dump_tree()
-// write code of each symbol to outfile
-// 	use write_code()
-// flush remaining code
-// 	use flush_codes()
-// close infile and outfile
