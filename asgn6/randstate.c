9eb160b40e9e2b2fa43f3f727c3efdf0094af2d5
Teresa Joseph <tkjoseph@ucsc.edu>
2021-11-11 02:21:34 +0000

    formatting

--- asgn6/randstate.c
+++ asgn6/randstate.c
@@ -6,6 +6,8 @@
 // random state interface for
 // the RSA library and number theory functions
 
-void randstate_init(uint64_t seed){}
+void randstate_init(uint64_t seed) {
+}
 
-void randstate_clear(void){}
+void randstate_clear(void) {
+}
