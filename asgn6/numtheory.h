e93530f22efaafd0c25857a0ce7e07641e425638
Teresa Joseph <tkjoseph@ucsc.edu>
2021-11-12 00:59:32 +0000

    Replace numtheory.h

--- asgn6/numtheory.h
+++ asgn6/numtheory.h
@@ -5,11 +5,11 @@
 #include <stdio.h>
 #include <gmp.h>
 
-void gcd(mpz_t g, mpz_t a, mpz_t b);
+void gcd(mpz_t d, mpz_t a, mpz_t b);
 
-void mod_inverse(mpz_t o, mpz_t a, mpz_t n);
+void mod_inverse(mpz_t i, mpz_t a, mpz_t n);
 
-void pow_mod(mpz_t o, mpz_t a, mpz_t d, mpz_t n);
+void pow_mod(mpz_t out, mpz_t base, mpz_t exponent, mpz_t modulus);
 
 bool is_prime(mpz_t n, uint64_t iters);
 
