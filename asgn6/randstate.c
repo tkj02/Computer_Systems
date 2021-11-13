72249a1e7837049ad13861eb86e6445eb9b8056f
Teresa Joseph <tkjoseph@ucsc.edu>
2021-11-13 20:19:11 +0000

    init and clear

--- asgn6/randstate.c
+++ asgn6/randstate.c
@@ -2,12 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Ccontains the implementation of the
+// Contains the implementation of the
 // random state interface for
 // the RSA library and number theory functions
 
+// Initializes state using seed
 void randstate_init(uint64_t seed) {
+	gmp_randinit_mt(state);
+	gmp_randseed_ui(state, seed);
 }
 
+// Clears and frees memory used by state
 void randstate_clear(void) {
+	 gmp_randclear(state);
 }
