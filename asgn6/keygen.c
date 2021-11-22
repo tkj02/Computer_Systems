d5e537687ca9cd250161dbbadb090e0e8af157b3
Teresa Joseph <tkjoseph@ucsc.edu>
2021-11-22 05:02:51 +0000

    mostly complete

--- asgn6/keygen.c
+++ asgn6/keygen.c
@@ -22,18 +22,32 @@ int main(int argc, char **argv) {
     int opt = 0;
     uint64_t iters = 50;
     bool v_flag = false;
-    uint64_t bits = 16;
+    uint64_t bits = 256;
+    FILE *pubfile = NULL;
+    FILE *privfile = NULL;
     unsigned long seed = time(NULL);
 
     // Parses through command line options
     while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
         switch (opt) {
-        case 'b': bits = atoi(optarg); break;
-        case 'c': iters = atoi(optarg); break;
-        case 'n': break;
-        case 'd': break;
-        case 's': seed = time(NULL); break;
-        case 'v': v_flag = true; break;
+        case 'b':
+		bits = atoi(optarg);
+		break;
+        case 'c':
+		iters = atoi(optarg);
+		break;
+        case 'n':
+		pubfile = fopen(optarg, "w");
+		break;
+        case 'd':
+		privfile = fopen(optarg, "w");
+		break;
+        case 's':
+		seed = time(NULL);
+		break;
+        case 'v':
+		v_flag = true;
+		break;
         case 'h':
             printf("SYNOPSIS\n   Generates an RSA public/private key pair.\n\n");
             printf("USAGE\n   ./keygen [-hv] [-b bits] -n pbfile -d pvfile\n\n");
@@ -49,17 +63,59 @@ int main(int argc, char **argv) {
             break;
         }
     }
+    // Opens public key file
+    if (pubfile == NULL){
+    	pubfile = fopen("rsa.pub", "w");
+    }
+
+    // Opens private key file
+    if (privfile == NULL){
+	    privfile = fopen("rsa.priv", "w");
+    }
+
+    // Initializes randstate with specified seed
     randstate_init(seed);
-    mpz_t p, q, e, n, d;
-    mpz_inits(p, q, e, n, d, NULL);
 
+    // Initializes mpz_t variables
+    mpz_t m, s, p, q, e, n, d;
+    mpz_inits(m, s, p, q, e, n, d, NULL);
+
+    // Makes public key and private key
     rsa_make_pub(p, q, n, e, bits, iters);
-    printf("key\n");
-    //rsa_make_priv(d, e, p, q);
-    (void) d;
+    rsa_make_priv(d, e, p, q);
 
-    // Freeing memory
-    mpz_clears(p, q, e, n, d, NULL);
+    // Gets username
+    char *user = getenv("USER");
+
+    // Converts username to mpz_t variable
+    mpz_init_set_str(m, user, 62);
+
+    // Computes signature of username
+    rsa_sign(s, m, d, n);
+
+    // Writes public key to public file
+    rsa_write_pub(n, e, s, user, pubfile);
+
+    // Writes private key to private file
+    rsa_write_priv(n, d, privfile);
+
+    // Checks verbose flag
+    if (v_flag){
+	    printf("user = %s\n", user);
+	    gmp_printf("s (%d bits) = %Zd\n", mpz_sizeinbase(s, 2), s);
+	    gmp_printf("p (%d bits) = %Zd\n", mpz_sizeinbase(p, 2), p);
+	    gmp_printf("q (%d bits) = %Zd\n", mpz_sizeinbase(q, 2), q);
+	    gmp_printf("n (%d bits) = %Zd\n", mpz_sizeinbase(n, 2), n);
+	    gmp_printf("e (%d bits) = %Zd\n", mpz_sizeinbase(e, 2), e);
+	    gmp_printf("d (%d bits) = %Zd\n", mpz_sizeinbase(d, 2), d);
+    }
+
+    // Closes files
+    fclose(pubfile);
+    fclose(privfile);
+
+    // Frees memory
+    mpz_clears(m, s, p, q, e, n, d, NULL);
     randstate_clear();
 
     return 0;
