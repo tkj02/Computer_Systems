19e39d0c2301267602e2aab6d973db01d04fa1d7
Teresa Joseph <tkjoseph@ucsc.edu>
2021-11-27 04:54:41 +0000

    complete (will check print later)

--- asgn7/bv.c
+++ asgn7/bv.c
@@ -1,16 +1,76 @@
 #include "bv.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-BitVector *bv_create(uint32_t length);
+struct BitVector {
+    uint32_t length;
+    uint8_t *vector;
+};
 
-void bv_delete(BitVector **bv);
+BitVector *bv_create(uint32_t length) {
+    BitVector *bv = (BitVector *) malloc(length);
+    if (bv == NULL) {
+        return NULL;
+    }
+    if (bv) {
+        bv->length = length;
+        for (uint32_t i = 0; i < length; i++) {
+            bv->vector[i] = 0;
+        }
+    }
+    return bv;
+}
 
-uint32_t bv_length(BitVector *bv);
+void bv_delete(BitVector **bv) {
+    free(*bv);
+    *bv = NULL;
+}
 
-bool bv_set_bit(BitVector *bv, uint32_t i);
+uint32_t bv_length(BitVector *bv) {
+    return bv->length;
+}
 
-bool bv_clr_bit(BitVector *bv, uint32_t i);
+bool bv_set_bit(BitVector *bv, uint32_t i) {
+    if (i >= bv->length) {
+        return false;
+    }
+    uint32_t byte_index = i / 8;
+    uint32_t bit_index = i % 8;
+    uint8_t mask = 1 << bit_index;
+    bv->vector[byte_index] |= mask;
+    return true;
+}
 
-bool bv_get_bit(BitVector *bv, uint32_t i);
+bool bv_clr_bit(BitVector *bv, uint32_t i) {
+    if (i >= bv->length) {
+        return false;
+    }
+    uint32_t byte_index = i / 8;
+    uint32_t bit_index = i % 8;
+    uint8_t mask = 1 << bit_index;
+    bv->vector[byte_index] &= (~mask);
+    return true;
+}
 
-void bv_print(BitVector *bv);
+bool bv_get_bit(BitVector *bv, uint32_t i) {
+    if (i >= bv->length) {
+        return false;
+    }
+    uint32_t byte_index = i / 8;
+    uint32_t bit_index = i % 8;
+    uint8_t mask = 1 << bit_index;
+    if (bv->vector[byte_index] & mask) {
+        return true;
+    }
+    return false;
+}
+
+void bv_print(BitVector *bv) {
+    for (uint32_t i = 0; i < bv->length; i++) {
+        if (bv->vector[i]) {
+            printf("%d\n", 1);
+        }
+        printf("%d\n", 0);
+    }
+}
