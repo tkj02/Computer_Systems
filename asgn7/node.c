73deeeb9544f35c48064873902bbffe731a8a35c
Teresa Joseph <tkjoseph@ucsc.edu>
2021-11-27 03:29:28 +0000

    complete

---
+++ asgn7/node.c
@@ -0,0 +1,38 @@
+#include "node.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Node *node_create(char *oldspeak, char *newspeak){
+	// Allocates memory for node
+	Node *n = (Node *) malloc(sizeof(Node));
+
+	// Sets left and right to NULL
+	// Allocates memory and copies old/new speak
+	if (n) {
+		n->left = NULL;
+		n->right = NULL;
+		n->oldspeak = strdup(oldspeak);
+        	n->newspeak = strdup(newspeak);
+	}
+	return n;
+}
+
+void node_delete(Node **n){
+	// Frees memory
+	free((*n)->oldspeak);
+	free((*n)->newspeak);
+	free(*n);
+	*n = NULL;
+}
+
+void node_print(Node *n){
+	// If oldspeak and newspeak exist, print both
+	if (n->oldspeak != NULL && n->newspeak != NULL){
+		printf("%s -> %s\n", n->oldspeak, n->newspeak);
+	}
+	// If only oldspeak exists, print it
+	else if (n->oldspeak != NULL && n->newspeak == NULL){
+		printf("%s\n", n->oldspeak);
+	}
+}
