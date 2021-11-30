43d4f050aba83756413a7dbd692b3e0e42b94ff2
Teresa Joseph <tkjoseph@ucsc.edu>
2021-11-30 05:21:25 +0000

    create and delete ht

--- asgn7/ht.c
+++ asgn7/ht.c
@@ -1,4 +1,5 @@
 #include "ht.h"
+#include "salts.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,42 +10,54 @@ struct HashTable {
     Node **trees;
 };
 
+HashTable *ht_create(uint32_t size) {
+    // Allocates memory for hash table
+    HashTable *ht = (HashTable *) malloc(sizeof(HashTable));
+    if (ht == NULL) {
+        return NULL;
+    }
+
+    // Sets salt values according to salts.h
+    ht->salt[0] = SALT_HASHTABLE_LO;
+    ht->salt[1] = SALT_HASHTABLE_HI;
+
+    // Sets hash table size to given size
+    ht->size = size;
+
+    // Allocates memory for trees
+    ht->trees = (Node **) malloc(size * sizeof(Node *));
+
+    return ht;
+}
+
+void ht_delete(HashTable **ht) {
+    // Frees tree memory
+    free((*ht)->trees);
+
+    // Frees hash table memory
+    free(*ht);
+    *ht = NULL;
+}
+
 #if 0
-
-HashTable *ht_create(uint32_t size){
-	HashTable *ht = malloc(size);
-
+uint32_t ht_size(HashTable *ht) {
 }
 
-void ht_delete(HashTable **ht){
-
+Node *ht_lookup(HashTable *ht, char *oldspeak) {
 }
 
-uint32_t ht_size(HashTable *ht){
-
+void ht_insert(HashTable *ht, char *oldspeak, char *newspeak) {
 }
 
-Node *ht_lookup(HashTable *ht, char *oldspeak){
-
+uint32_t ht_count(HashTable *ht) {
 }
 
-void ht_insert(HashTable *ht, char *oldspeak, char *newspeak){
-
+double ht_avg_bst_size(HashTable *ht) {
 }
 
-uint32_t ht_count(HashTable *ht){
-
+double ht_avg_bst_height(HashTable *ht) {
 }
 
-double ht_avg_bst_size(HashTable *ht){
-
-}
-
-double ht_avg_bst_height(HashTable *ht){
-
-}
-
-void ht_print(HashTable *ht){
-
+void ht_print(HashTable *ht) {
 }
 #endif
