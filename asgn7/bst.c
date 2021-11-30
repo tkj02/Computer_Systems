49e321a2b0090a018628ba73e9119681afc05e5d
Teresa Joseph <tkjoseph@ucsc.edu>
2021-11-30 23:52:48 +0000

    comments and find function fix

--- asgn7/bst.c
+++ asgn7/bst.c
@@ -5,18 +5,13 @@
 #include <string.h>
 
 Node *bst_create(void) {
-    Node *n = (Node *) malloc(sizeof(Node));
-    if (n == NULL) {
-        return n;
-    }
-    n->oldspeak = NULL;
-    n->newspeak = NULL;
-    n->left = NULL;
-    n->right = NULL;
-    return n;
+    // Returns empty tree
+    return NULL;
 }
 
+// Helper function for bst_height
 uint32_t max(uint32_t a, uint32_t b) {
+    // Returns greatest value between a and b
     if (a >= b) {
         return a;
     }
@@ -24,18 +19,23 @@ uint32_t max(uint32_t a, uint32_t b) {
 }
 
 uint32_t bst_height(Node *root) {
+    // If root is not NULL, returns max height + root height
     if (root) {
         return 1 + max(bst_height(root->left), bst_height(root->right));
     }
+
+    // Root is NULL and the height is zero
     return 0;
 }
 
 uint32_t bst_size(Node *root) {
+    // Keeps track of size
     uint32_t counter = 0;
+
+    // Increases count for every recursive call
     if (root) {
         counter += 1;
         counter += bst_size(root->left);
-
         counter += bst_size(root->right);
     }
     return counter;
@@ -50,6 +50,7 @@ Node *bst_find(Node *root, char *oldspeak) {
 
         // Current node is greater than oldspeak
         else if (strcmp(root->oldspeak, oldspeak) > 0) {
+            // Traverses to left of tree as new root
             Node *temp;
             temp = bst_find(root->left, oldspeak);
             return temp;
@@ -57,6 +58,7 @@ Node *bst_find(Node *root, char *oldspeak) {
 
         // Current node is less than oldspeak
         else if (strcmp(root->oldspeak, oldspeak) < 0) {
+            // Traverses to right of tree as new root
             return bst_find(root->right, oldspeak);
         }
 
@@ -64,25 +66,35 @@ Node *bst_find(Node *root, char *oldspeak) {
         else {
             return NULL;
         }
+
+        // Root is NULL
     } else {
-        return root;
+        return NULL;
     }
 }
 
 Node *bst_insert(Node *root, char *oldspeak, char *newspeak) {
+    // If bst does not already exist:
+
+    // Creates root of new bst
     if (root == NULL) {
         return node_create(oldspeak, newspeak);
     }
 
+    // Duplicates oldspeak to root's oldspeak
     if (root->oldspeak == NULL) {
         root->oldspeak = strdup(oldspeak);
 
+        // If newspeak exists, duplicates it to root's newspeak
         if (newspeak != NULL) {
             root->newspeak = strdup(newspeak);
         }
 
+        // Returns root of new bst
         return root;
     }
+
+    // If bst already exists:
 
     // Checks if oldspeak and newspeak are in bst
     // If so, does not insert and returns NULL
@@ -90,11 +102,15 @@ Node *bst_insert(Node *root, char *oldspeak, char *newspeak) {
         return NULL;
     }
 
+    // Current node is greater than oldspeak
     else if (strcmp(root->oldspeak, oldspeak) > 0) {
+        // Traverses to left of tree as new root
         root->left = bst_insert(root->left, oldspeak, newspeak);
     }
 
+    // Current node is lss than oldspeak
     else if (strcmp(root->oldspeak, oldspeak) < 0) {
+        // Traverses to right of tree as new root
         root->right = bst_insert(root->right, oldspeak, newspeak);
     }
 
@@ -102,6 +118,7 @@ Node *bst_insert(Node *root, char *oldspeak, char *newspeak) {
 }
 
 void bst_print(Node *root) {
+    // If root is not NULL, prints nodes in inorder
     if (root) {
         bst_print(root->left);
         node_print(root);
@@ -110,6 +127,7 @@ void bst_print(Node *root) {
 }
 
 void bst_delete(Node **root) {
+    // Recursively frees node memory in postorder
     if (*root == NULL) {
         return;
     }
