03e4af92132f96a7de79e53bbab3a64d3e0d8fde
Teresa Joseph <tkjoseph@ucsc.edu>
2021-12-01 00:01:04 +0000

    attempt at fixing seg fault

--- asgn7/bv.c
+++ asgn7/bv.c
@@ -11,7 +11,7 @@ struct BitVector {
 
 BitVector *bv_create(uint32_t length) {
     // Allocates memory for bit vector
-    BitVector *bv = (BitVector *) malloc(sizeof(BitVector));
+    BitVector *bv = (BitVector *) malloc(length * sizeof(BitVector));
     if (bv == NULL) {
         return NULL;
     }
